4ebb4c43671a21a87895de407294d62c7a4464b6
mark <mark.funkhouser@gmail.com>
2024-01-28 16:59:38 -0800

    render preview of raw pdf and modified pdf

--- pdf-flipper/src/components/App.js
+++ pdf-flipper/src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Main from './Main';
-import styles from './styles.less';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <Main></Main>
+                <Main />
             </div>
         );
     }
--- pdf-flipper/src/components/Main.js
+++ pdf-flipper/src/components/Main.js
@@ -1,33 +1,54 @@
-import React, { useRef, useState } from 'react';
-import styles from './styles.less';
-import { PDFDocument } from 'pdf-lib';
-import { loadPDF, foldPDF } from '../lib/pdf.mjs';
-import { PdfViewer } from './PdfViewer';
-import { Document, Page, pdfjs } from 'react-pdf';
+import React, { useRef, useState } from "react";
+import { Box, Button } from "@mui/material";
+import { Document, Page, pdfjs } from "react-pdf";
+import { loadPDF, foldPDF } from "../lib/pdf.mjs";
 
 const Main = () => {
     const fileInputRef = useRef(null);
     const [rawPDF, setRawPDF] = useState(null);
+    const [foldedPDF, setFoldedPDF] = useState(null);
     const [numPages, setNumPages] = useState(null);
+    const [numPagesFolded, setNumPagesFolded] = useState(null);
+
     const [pageNumber, setPageNumber] = useState(1);
+    const [pageNumberFolded, setPageNumberFolded] = useState(1);
+
 
     pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-        'pdfjs-dist/build/pdf.worker.min.js',
-        import.meta.url,
-      ).toString();
-  
-    const onDocumentLoadSuccess = ({ numberOfPages }) => {
-      setNumPages(numberOfPages);
-    }
+        "pdfjs-dist/build/pdf.worker.min.js",
+        import.meta.url
+    ).toString();
+
+    const onDocumentLoadSuccessRaw = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+
+    const onDocumentLoadSuccessFolded = ({ numPages }) => {
+        setNumPagesFolded(numPages);
+    };
+
+    const decrement = () => {
+        setPageNumber(pageNumber - 1);
+    };
+
+    const increment = () => {
+        setPageNumber(pageNumber + 1);
+    };
+
+    const decrementFolded = () => {
+        setPageNumberFolded(pageNumberFolded - 1);
+    };
+
+    const incrementFolded = () => {
+        setPageNumberFolded(pageNumberFolded + 1);
+    };
 
     const handleButtonClick = () => {
         fileInputRef.current.click();
     };
-    
+
     const handleFileChange = async (event) => {
         const file = event.target.files[0];
-
-
         if (file) {
             try {
                 setRawPDF(file);
@@ -39,64 +60,93 @@ const Main = () => {
                 // foldPDF returns as 'bytes'
                 const foldedPdf = await foldPDF(loadedPdfData);
                 console.log(foldedPdf);
-
+                
                 // Create a blob from the bytes
-                const blob = new Blob([foldedPdf], { type: 'application/pdf' });
-    
+                const blob = new Blob([foldedPdf], { type: "application/pdf" });
+                setFoldedPDF(blob);
+
                 // Create a URL for the blob
                 const url = URL.createObjectURL(blob);
-    
+
                 // Create a temporary link element and trigger the download
-                const link = document.createElement('a');
+                const link = document.createElement("a");
                 link.href = url;
-                link.download = 'folded-pdf.pdf'; // Name the download file
+                link.download = "folded-pdf.pdf"; // Name the download file
                 document.body.appendChild(link);
                 link.click();
-    
+
                 // Clean up
                 document.body.removeChild(link);
                 URL.revokeObjectURL(url);
-                
             } catch (error) {
-                console.error('Error processing file:', error);
+                console.error("Error processing file:", error);
             }
         }
     };
-    
     return (
-        <div class={styles.Main}>
-            <div>
-                <h1>Hello, Preact!</h1>
-            </div>
-            <div>
-                <button onClick={handleButtonClick}>Upload PDF</button>
+        <Box id="main">
+            <Box>
+                <h1>PDF Flipper</h1>
+            </Box>
+            <Box>
+                <Button onClick={handleButtonClick}>Upload PDF</Button>
                 <input
                     type="file"
                     ref={fileInputRef}
                     onChange={handleFileChange}
-                    style={{ display: 'none' }}
+                    style={{ display: "none" }}
                     accept="application/pdf"
                 />
-            </div>
-            <div>
-                Upload the PDF
-            </div>
-            <div>
-                Click the button
-            </div>
-            <div>
-                Download the things
-            </div>
-
-
-
-        <Document file={rawPDF} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
-        </Document>
-
-
-
-        </div>
+            </Box>
+            <Box>Upload the PDF</Box>
+            <Box>Click the button</Box>
+            <Box>Download the things</Box>
+            <Box display="flex" flexDirection="row">
+                <Box id="testRaw" width="200px" height="600px">
+                    <Document
+                        file={rawPDF}
+                        onLoadSuccess={onDocumentLoadSuccessRaw}
+                    >
+                        <Box display={"flex"} flexDirection={"row"}>
+                            <Page
+                                pageNumber={pageNumber}
+                                renderAnnotationLayer={false}
+                                renderTextLayer={false}
+                            />
+                            <Page
+                                pageNumber={pageNumber + 1}
+                                renderAnnotationLayer={false}
+                                renderTextLayer={false}
+                            />
+                        </Box>
+                    </Document>
+                    <Button onClick={decrement}> - </Button>
+                    <Button onClick={increment}> + </Button>
+                </Box>
+                <Box width="1000px"/>
+                <Box id="testFolded" width="200px" height="600px">
+                    <Document
+                        file={foldedPDF}
+                        onLoadSuccess={onDocumentLoadSuccessFolded}
+                    >
+                        <Box display={"flex"} flexDirection={"row"}>
+                            <Page
+                                pageNumber={pageNumberFolded}
+                                renderAnnotationLayer={false}
+                                renderTextLayer={false}
+                            />
+                            <Page
+                                pageNumber={pageNumberFolded + 1}
+                                renderAnnotationLayer={false}
+                                renderTextLayer={false}
+                            />
+                        </Box>
+                    </Document>
+                    <Button onClick={decrementFolded}> - </Button>
+                    <Button onClick={incrementFolded}> + </Button>
+                </Box>
+            </Box>
+        </Box>
     );
 };
 
