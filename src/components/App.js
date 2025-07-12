dad66a0ce6978d7bab2beb028fb138d08eea1a47
Brad Arnett <brad.arnett@gmail.com>
2025-07-12 11:06:32 -0700

    fix router for ghpages

--- src/components/App.js
+++ src/components/App.js
@@ -37,7 +37,7 @@ export default class App extends React.Component {
     render() {
         return (
             <ThemeProvider theme={theme}>
-            <Router>
+            <Router basename="/">
               <Routes>
                 <Route path="/" element={
                   <AppProvider>
